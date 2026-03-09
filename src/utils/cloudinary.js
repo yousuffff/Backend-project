e94fa42c5efb495a0f7bf115e65f7393578d4f5a
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-03-09 21:07:58 +0530

    cloudinary added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_NAME,
+});
+
+const uploadOnCloudinary = async (localfilePath) => {
+  try {
+    if (!localfilePath) return null;
+    const response = await cloudinary.uploader.upload(localfilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfull
+    //console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localfilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localfilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
