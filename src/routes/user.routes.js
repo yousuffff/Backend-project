f7afcc54b750fef1433e113af679713a7a9baa9d
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-04-02 23:19:03 +0530

    feat: reorganize user routes for improved structure and readability

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import {
+  changePassword,
+  getCurrentUser,
+  getUserProfile,
+  getWatchHistory,
   loginUser,
   logOutUser,
   refreshAccessToken,
   registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  updateUserDetail,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
-
 const router = Router();
-
 
 router.route("/register").post(
   upload.fields([
@@ -26,11 +31,28 @@ router.route("/register").post(
   registerUser
 );
 
-
 router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logOutUser);
+
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changePassword);
+
+router.route("/get-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateUserDetail);
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/user-profile/:username").get(verifyJWT, getUserProfile);
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
