19505bcd7b832faefebed2f9a849234fd8f8f559
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-04-10 02:13:54 +0530

    feat: implement toggle like functionality for tweets with validation and unliking capability; enhance liked videos retrieval with pagination

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Video } from "../models/video.model.js";
+import { Tweet } from "../models/tweets.model.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
@@ -74,10 +75,104 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 const toggleTweetLike = asyncHandler(async (req, res) => {
   const { tweetId } = req.params;
   //TODO: toggle like on tweet
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet id");
+  }
+  const userId = req.user._id;
+
+  const tweetExist = await Tweet.findById(tweetId);
+  if (!tweetExist) {
+    throw new ApiError(404, "Tweet not Found");
+  }
+
+  const isLiked = await Like.findOne({
+    tweet: tweetId,
+    likedBy: userId,
+  });
+  if (isLiked) {
+    await Like.deleteOne({
+      tweet: tweetId,
+      likedBy: userId,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Tweet unliked successfully"));
+  }
+
+  const newLike = await Like.create({
+    tweet: tweetId,
+    likedBy: userId,
+  });
+  return res
+    .status(201)
+    .json(new ApiResponse(201, newLike, "Tweet liked successfully"));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
   //TODO: get all liked videos
+  const userId = new mongoose.Types.ObjectId(req.user?._id);
+  const { page = 1, limit = 10 } = req.query;
+
+  const pipeline = [
+    {
+      $match: {
+        likedBy: userId,
+        video: { $exists: true },
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "video",
+      },
+    },
+    {
+      $addFields: {
+        video: {
+          $first: "$video",
+        },
+      },
+    }, // ❌ Remove invalid videos (optional safety)
+    {
+      $match: {
+        "video._id": { $exists: true },
+      },
+    },
+    // 🔥 Sort by latest liked
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        likedAt: "$createdAt",
+        "video._id": 1,
+        "video.title": 1,
+        "video.thumbnail": 1,
+        "video.duration": 1,
+      },
+    },
+  ];
+  const options = {
+    limit: parseInt(limit) || 10,
+    page: parseInt(page) || 1,
+  };
+
+  const likedVideos = await Like.aggregatePaginate(pipeline, options);
+  if (!likedVideos) {
+    throw new ApiError(500, "Something Went wrong");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, likedVideos, "Liked video fetched successfully")
+    );
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
