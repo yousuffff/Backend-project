1b5c22ec2387b584f3b2675b8e6bdf94b20ae559
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-04-09 16:36:45 +0530

    feat: implement video like toggle functionality with validation and unliking capability

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,10 +3,41 @@ import { Like } from "../models/like.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+  const userId = req.user?._id;
+  const videoExist = await Video.findById(videoId);
+  if (!videoExist) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  //checking if it already liked
+  const isLiked = await Like.findOne({
+    video: videoId,
+    likedBy: userId,
+  });
+
+  if (isLiked) {
+    await Like.deleteOne({ _id: isLiked._id });
+
+    return res.status(200).json(new ApiResponse(200, {}, "Video Unliked"));
+  }
+
   //TODO: toggle like on video
+  // like
+  const like = await Like.create({
+    video: videoId,
+    likedBy: userId,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, like, "Video liked successfully"));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
--- src/models/like.model.js
+++ src/models/like.model.js
@@ -13,6 +13,7 @@ const likeSchema = new Schema(
     likedBy: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
     tweet: {
       type: Schema.Types.ObjectId,
