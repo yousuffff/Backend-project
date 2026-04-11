edd8d68685fbff7c1709dda1b703f29ced2cf790
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-04-11 16:18:25 +0530

    feat: add placeholder for removeVideoFromPlaylist function implementation

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -187,6 +187,8 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
   // TODO: remove video from playlist
+
+  
 });
 
 const deletePlaylist = asyncHandler(async (req, res) => {
